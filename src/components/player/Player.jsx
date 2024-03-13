c9b6073171f5f833b2195dd9629a8d47e9058d1a
Akash Deshpande <akashdeshpande118@gmail.com>
2024-03-13 20:04:08 +0530

    Add seek, volume and speed in player

--- src/components/player/Player.jsx
+++ src/components/player/Player.jsx
@@ -3,8 +3,12 @@ import { useState } from "react";
 
 function Player(props) {
 
+    // https://howlerjs.com/assets/howler.js/examples/player/audio/rave_digger.webm
+
     const [mediaUrl, setMediaUrl] = useState("");
     const [sound, setSound] = useState(null);
+    const [volume, setVolume] = useState(1.0);
+    const [speed, setSpeed] = useState(1.0);
 
     function playSound() {
         if(sound) {
@@ -16,7 +20,8 @@ function Player(props) {
                 src: mediaUrl,
                 preload: true,
                 onend: () => console.log("playback finished"),
-                onloaderror: (id, error) => console.error("Error loading sound source: ", error )
+                onloaderror: (id, error) => console.error("Error loading sound source: ", error ),
+                html5: true
             });
 
             setSound(sound);
@@ -35,6 +40,30 @@ function Player(props) {
             sound.stop();
     }
 
+    function seekForward() {
+        if(sound) {
+            sound.seek((sound.seek() + 5));
+        }
+    }
+
+    function seekBackward() {
+         if(sound) {
+            sound.seek((sound.seek() - 5));
+        }   
+    }
+
+    function handleVolumeChange(event) {
+        setVolume(event.target.value);
+        if(sound)
+            sound.volume(event.target.value);
+    }
+
+    function handleSpeedChange(event) {
+        setSpeed(event.target.value);
+        if(sound)
+            sound.rate(event.target.value)  
+    }
+
     return <>
         <p>Input URL for sound:</p>
         <input type="text" id="mediaUrl" onChange={(e) => setMediaUrl(e.target.value) } value={mediaUrl}/>
@@ -45,6 +74,17 @@ function Player(props) {
         <button onClick={playSound}>Play</button>
         <button onClick={pauseSound}>Pause</button>
         <button onClick={stopSound}>Stop</button>
+
+        <button onClick={seekForward}>Seek Forward (+5s)</button>
+        <button onClick={seekBackward}>Seek Backward(-5s)</button>
+
+        <br />
+        <p>Volume: {Math.floor(volume * 100)}</p>
+        <input type="range" name="volume" id="volume" value={volume} onChange={handleVolumeChange} min={0.0} max={1.0} step={0.01}/>
+
+        <br />
+        <p>Speed: {speed}x</p>
+        <input type="range" name="speed" id="speed" value={speed} onChange={handleSpeedChange} min={0.5} max={4.0} step={0.5}/>
     </>;
 }
 
